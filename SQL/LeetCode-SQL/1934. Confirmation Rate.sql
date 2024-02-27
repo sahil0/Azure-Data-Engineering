e7fe43b64c42017c7198c19817ce7ba43fb08e8b
Sahil Mahale <sahilmahale0@gmail.com>
2024-02-27 15:48:03 +0530

    Update 1934. Confirmation Rate.sql

--- SQL/LeetCode-SQL/1934. Confirmation Rate.sql
+++ SQL/LeetCode-SQL/1934. Confirmation Rate.sql
@@ -100,3 +100,7 @@ group by S.user_id
 --   Confirmations c ON s.user_id = c.user_id
 -- GROUP BY
 --   s.user_id;
+
+SELECT s.user_id, round(ifnull(avg(c.action = 'confirmed'), 0),2) as confirmation_rate
+from signups s left join confirmations c on s.user_id = c.user_id
+group by user_id
