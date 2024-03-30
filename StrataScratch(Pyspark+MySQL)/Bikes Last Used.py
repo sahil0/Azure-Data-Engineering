300bc790988dfc2b788ea0c529e6fce7c5c55301
Sahil Mahale <sahilmahale0@gmail.com>
2024-03-30 16:56:50 +0530

    Update Bikes Last Used.py

--- StrataScratch(Pyspark+MySQL)/Bikes Last Used.py
+++ StrataScratch(Pyspark+MySQL)/Bikes Last Used.py
@@ -8,3 +8,12 @@ import pyspark
 from pyspark.sql.functions import col,max,desc
 # Start writing code
 dc_bikeshare_q1_2012=dc_bikeshare_q1_2012.groupBy("bike_number").agg(max("end_time").alias("last_used")).orderBy(desc("last_used"))
+
+
+Mysql:
+WITH LastUsed AS(select  DISTINCT bike_number,
+end_time AS last_used,
+dense_rank() over(PARTITION By bike_number order By end_time desc) AS  rnk
+from dc_bikeshare_q1_2012
+)
+select bike_number,last_used from LastUsed where rnk=1 order by last_used desc
