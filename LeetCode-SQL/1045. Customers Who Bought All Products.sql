61833aafdbdfa7588d9ccbdee4936e848527b6de
Sahil Mahale <sahilmahale0@gmail.com>
2024-03-31 14:21:32 +0530

    Create 1045. Customers Who Bought All Products.sql

---
+++ LeetCode-SQL/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,72 @@
+Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| customer_id | int     |
+| product_key | int     |
++-------------+---------+
+This table may contain duplicates rows. 
+customer_id is not NULL.
+product_key is a foreign key (reference column) to Product table.
+ 
+
+Table: Product
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_key | int     |
++-------------+---------+
+product_key is the primary key (column with unique values) for this table.
+ 
+
+Write a solution to report the customer ids from the Customer table that bought all the products in the Product table.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customer table:
++-------------+-------------+
+| customer_id | product_key |
++-------------+-------------+
+| 1           | 5           |
+| 2           | 6           |
+| 3           | 5           |
+| 3           | 6           |
+| 1           | 6           |
++-------------+-------------+
+Product table:
++-------------+
+| product_key |
++-------------+
+| 5           |
+| 6           |
++-------------+
+Output: 
++-------------+
+| customer_id |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: 
+The customers who bought all the products (5 and 6) are customers with IDs 1 and 3.
+
+
+# Write your MySQL query statement below
+SELECT C.customer_id  FROM Customer AS C 
+JOIN Product AS P ON P.product_key=C.product_key
+group by C.customer_id 
+having count(distinct C.product_key)=(select count(DISTINCT product_key) from Product)
+
+SELECT CUSTOMER_ID AS customer_id
+FROM CUSTOMER
+GROUP BY CUSTOMER_ID
+HAVING (COUNT(DISTINCT(PRODUCT_KEY))=(SELECT COUNT(PRODUCT_KEY) FROM PRODUCT))
